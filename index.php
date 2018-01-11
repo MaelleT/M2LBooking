19bfea9c57bdc5649ad53280d71e1511ed3952e2
MaelleT <maelle.taurand@ac-nantes.fr>
2018-01-11 21:15:12 +0100

    Structure du site : entête - pied de page - CSS

--- index.php
+++ index.php
@@ -4,3 +4,21 @@
  * Date: 10/01/18
  * V1.0
  */
+
+include("vues/v_entete.php");
+?>
+
+<!-- Begin page content -->
+<div class="container">
+
+    <div class="page-header">
+        <h3>Système de réservations de salles</h3>
+    </div>
+    <p class="lead">Bienvenue</p>
+</div>
+
+
+<?php
+include("vues/v_pied.php") ;
+
+?>
---
+++ vues/v_entete.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Portail de la M2L</title>
+
+    <!-- Bootstrap -->
+    <link href="public/css/bootstrap.min.css" rel="stylesheet" >
+    <link href="public/css/sticky-footer-navbar.css" rel="stylesheet" >
+
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+
+<!-- Fixed navbar -->
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">M2L</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Planings</a></li>
+                <li><a href="#about">Réserver</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Espace réservé <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Voir mes réservations</a></li>
+                        <li><a href="#">Annuler une réservation</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li class="dropdown-header">Admin</li>
+                        <li><a href="#">Ajouter une salle</a></li>
+                        <li><a href="#">Modifier une salle</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div><!--/.nav-collapse -->
+    </div>
+</nav>
+
+
+
